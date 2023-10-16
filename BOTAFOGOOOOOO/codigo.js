a7486d01a982f9c254ee02ee6dece64206e891de
GuilhermeVA <133550414+GuilhermeVA@users.noreply.github.com>
2023-10-16 08:55:40 -0300

    Add files via upload

--- BOTAFOGOOOOOO/codigo.js
+++ BOTAFOGOOOOOO/codigo.js
@@ -1,5 +1,27 @@
-const ingrid = atletas[0];
+const body = document.body;
+const div_container = document.createElement('div');
+div_container.id = 'container';
+body.appendChild(div_container);
 
+const cria_cartao = (atleta) => {
+
+const container_atleta = document.createElement('div');
+const titulo = document.createElement('h3');
+titulo.innerHTML = atleta.nome;
+const imagens = document.createElement('img');
+imagens.src = atleta.imagem;
+const descricao = document.createElement('p');
+descricao.innerHTML = atleta.descricao;
+
+container_atleta.appendChild(titulo)
+container_atleta.appendChild(imagens)
+container_atleta.appendChild(descricao)
+
+div_container.appendChild(container_atleta)
+}
+atletas.forEach((atleta) => (cria_cartao(atleta)))
+
+/*
 const espaco_nome = document.getElementById('nome');
 const imagem = document.querySelector('#container > img');
 const espaco_descricao = document.querySelector('#container > p');
@@ -11,4 +33,4 @@ imagem.src = atleta.imagem
 espaco_descricao.innerText = atleta.descricao
 
 }
-preenche(atletas[3])
+preenche(atletas[3]) */
